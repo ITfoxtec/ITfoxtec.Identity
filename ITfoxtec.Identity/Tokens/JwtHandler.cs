fdc7b5e14b9b98a9de8144971a7031dd60768458
Anders Revsgaard <anders@itfoxtec.com>
2019-02-01 15:51:40 +0100

    Core 2.0 support. Create Token support audiances. .NET 6.4.2 support removed.

--- ITfoxtec.Identity/Tokens/JwtHandler.cs
+++ ITfoxtec.Identity/Tokens/JwtHandler.cs
@@ -6,13 +6,20 @@ using msJwt = System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography.X509Certificates;
 using ITfoxtec.Identity.Discovery;
+using System.Linq;
 
 namespace ITfoxtec.Identity.Tokens
 {
     public class JwtHandler
     {
-        public static msJwt.JwtSecurityToken CreateToken(JsonWebKey jwk, string issuer, string audience, IEnumerable<Claim> claims, DateTimeOffset? issuedAt = null, int beforeIn = 60, int expiresIn = 3600, string algorithm = IdentityConstants.Algorithms.Asymmetric.RS256)
+        public static msJwt.JwtSecurityToken CreateToken(JsonWebKey jwk, string issuer, string audiences, IEnumerable<Claim> claims, DateTimeOffset? issuedAt = null, int beforeIn = 60, int expiresIn = 3600, string algorithm = IdentityConstants.Algorithms.Asymmetric.RS256)
         {
+            return CreateToken(jwk, issuer, new[] { audiences }, claims, issuedAt, beforeIn, expiresIn, algorithm);
+        }
+        public static msJwt.JwtSecurityToken CreateToken(JsonWebKey jwk, string issuer, IEnumerable<string> audiences, IEnumerable<Claim> claims, DateTimeOffset? issuedAt = null, int beforeIn = 60, int expiresIn = 3600, string algorithm = IdentityConstants.Algorithms.Asymmetric.RS256)
+        {
+            if (audiences?.Count() < 1) throw new ArgumentException($"At least one audience is required.", "audience");
+
             var header = new msJwt.JwtHeader(new msTokens.SigningCredentials(ConvertJsonWebKey(jwk), algorithm));
             if (!jwk.X509CertificateSHA1Thumbprint.IsNullOrEmpty())
             {
@@ -23,8 +30,14 @@ namespace ITfoxtec.Identity.Tokens
             {
                 issuedAt = DateTimeOffset.UtcNow;
             }
-            var payload = new msJwt.JwtPayload(issuer, audience, claims, issuedAt.Value.AddSeconds(-beforeIn).UtcDateTime, issuedAt.Value.AddSeconds(expiresIn).UtcDateTime, issuedAt.Value.UtcDateTime);
-
+            var payload = new msJwt.JwtPayload(issuer, audiences.First(), claims, issuedAt.Value.AddSeconds(-beforeIn).UtcDateTime, issuedAt.Value.AddSeconds(expiresIn).UtcDateTime, issuedAt.Value.UtcDateTime);
+            if (audiences.Count() > 1)
+            {
+                foreach (var audience in audiences.Skip(1))
+                {
+                    payload.AddClaim(new Claim(JwtClaimTypes.Audience, audience));
+                } 
+            }
             return new msJwt.JwtSecurityToken(header, payload);
         }
 
