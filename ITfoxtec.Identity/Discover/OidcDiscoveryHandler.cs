d8600ae98da7b732313983886fa4f353233242db
Anders Revsgaard <anders@itfoxtec.com>
2019-07-16 11:06:30 +0200

    OidcDiscoveryHandler support a list of documents.

--- ITfoxtec.Identity/Discover/OidcDiscoveryHandler.cs
+++ ITfoxtec.Identity/Discover/OidcDiscoveryHandler.cs
@@ -1,81 +1,160 @@
 ﻿using Microsoft.IdentityModel.Tokens;
 using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ITfoxtec.Identity.Discovery
 {
-    public class OidcDiscoveryHandler
+    /// <summary>
+    /// Call OIDC Discovery and cache result.
+    /// </summary>
+    public class OidcDiscoveryHandler : IDisposable
     {
+        private readonly CancellationTokenSource cleanUpCancellationTokenSource;
         private readonly IHttpClientFactory httpClientFactory;
-        private readonly string oidcDiscoveryUri;
-        private readonly int expiresIn;
-        private OidcDiscovery oidcDiscovery;
-        private JsonWebKeySet jsonWebKeySet;
-        private DateTimeOffset oidcDiscoveryValidUntil = DateTimeOffset.MinValue;
-        private DateTimeOffset jsonWebKeySetValidUntil = DateTimeOffset.MinValue;
+        private readonly string defaultOidcDiscoveryUri;
+        private readonly int defaultExpiresIn;
+        private Dictionary<string, (OidcDiscovery, DateTimeOffset)> oidcDiscoveryCache = new Dictionary<string, (OidcDiscovery, DateTimeOffset)>();
+        private Dictionary<string, (JsonWebKeySet, DateTimeOffset)> jsonWebKeySetCache = new Dictionary<string, (JsonWebKeySet, DateTimeOffset)>();
 
-        public OidcDiscoveryHandler(IHttpClientFactory httpClientFactory, string oidcDiscoveryUri, int expiresIn = 3600)
+        /// <summary>
+        /// Call OIDC Discovery and cache result.
+        /// </summary>
+        /// <param name="httpClientFactory">The IHttpClientFactory instance.</param>
+        /// <param name="defaultOidcDiscoveryUri">The default OIDC Discovery uri.</param>
+        /// <param name="defaultExpiresIn">The default expires in seconds.</param>
+        public OidcDiscoveryHandler(IHttpClientFactory httpClientFactory, string defaultOidcDiscoveryUri = null, int defaultExpiresIn = 3600)
         {
             this.httpClientFactory = httpClientFactory;
-            this.oidcDiscoveryUri = oidcDiscoveryUri;
-            this.expiresIn = expiresIn;
+            this.defaultOidcDiscoveryUri = defaultOidcDiscoveryUri;
+            this.defaultExpiresIn = defaultExpiresIn;
+
+            cleanUpCancellationTokenSource = new CancellationTokenSource();
+            Task.Factory.StartNew(async () => { await CleanOldCacheItems(); }, cleanUpCancellationTokenSource.Token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
         }
 
-        public async Task<OidcDiscovery> GetOidcDiscoveryAsync()
+        private async Task CleanOldCacheItems()
         {
-            if (oidcDiscovery == null || oidcDiscoveryValidUntil < DateTimeOffset.UtcNow)
+            while (true)
             {
-                var request = new HttpRequestMessage(HttpMethod.Get, oidcDiscoveryUri);
+                var ct = cleanUpCancellationTokenSource.Token;
+                ct.ThrowIfCancellationRequested();
+                await Task.Delay(new TimeSpan(0, 5, 0), ct);
 
-                var client = httpClientFactory.CreateClient();
-                using (var response = await client.SendAsync(request))
+                ct.ThrowIfCancellationRequested();
+                foreach (var item in oidcDiscoveryCache)
                 {
-                    // Handle the response
-                    switch (response.StatusCode)
+                    (var oidcDiscovery, var oidcDiscoveryValidUntil) = item.Value;
+                    if (oidcDiscoveryValidUntil < DateTimeOffset.UtcNow)
                     {
-                        case HttpStatusCode.OK:
-                            var result = await response.Content.ReadAsStringAsync();
-                            oidcDiscovery = result.ToObject<OidcDiscovery>();
-                            oidcDiscoveryValidUntil = DateTimeOffset.UtcNow.AddSeconds(expiresIn);
-                            break;
+                        oidcDiscoveryCache.Remove(item.Key);
+                    }
+                }
 
-                        default:
-                            throw new Exception($"Error, Status Code OK expected. StatusCode={response.StatusCode}");
+                ct.ThrowIfCancellationRequested();
+                foreach (var item in jsonWebKeySetCache)
+                {
+                    (var jsonWebKeySet, var jsonWebKeySetValidUntil) = item.Value;
+                    if (jsonWebKeySetValidUntil < DateTimeOffset.UtcNow)
+                    {
+                        jsonWebKeySetCache.Remove(item.Key);
                     }
                 }
             }
-
-            return oidcDiscovery;
         }
 
-        public async Task<JsonWebKeySet> GetOidcDiscoveryKeysAsync()
+        /// <summary>
+        /// Call OIDC Discovery endpoint or read the OIDC Discovery from the cache.
+        /// </summary>
+        /// <param name="oidcDiscoveryUri">The OIDC Discovery uri. If not specified the default OIDC Discovery uri is used.</param>
+        /// <param name="expiresIn">The expires in seconds. If not specified the default expires in is used.</param>
+        /// <returns>Return OIDC Discovery result.</returns>
+        public async Task<OidcDiscovery> GetOidcDiscoveryAsync(string oidcDiscoveryUri = null, int? expiresIn = null)
         {
-            if (jsonWebKeySet == null || jsonWebKeySetValidUntil < DateTimeOffset.UtcNow)
+            oidcDiscoveryUri = oidcDiscoveryUri ?? defaultOidcDiscoveryUri;
+            expiresIn = expiresIn ?? defaultExpiresIn;
+
+            if(oidcDiscoveryCache.ContainsKey(oidcDiscoveryUri))
             {
-                await GetOidcDiscoveryAsync();
-                var request = new HttpRequestMessage(HttpMethod.Get, oidcDiscovery.JwksUri);
-
-                var client = httpClientFactory.CreateClient();
-                using (var response = await client.SendAsync(request))
+                (var oidcDiscovery, var oidcDiscoveryValidUntil) = oidcDiscoveryCache[oidcDiscoveryUri];
+                if(oidcDiscoveryValidUntil >= DateTimeOffset.UtcNow)
                 {
-                    // Handle the response
-                    switch (response.StatusCode)
-                    {
-                        case HttpStatusCode.OK:
-                            var result = await response.Content.ReadAsStringAsync();
-                            jsonWebKeySet = result.ToObject<JsonWebKeySet>();
-                            jsonWebKeySetValidUntil = DateTimeOffset.UtcNow.AddSeconds(expiresIn);
-                            break;
-
-                        default:
-                            throw new Exception($"Error, Status Code OK expected. StatusCode={response.StatusCode}");
-                    }
+                    return oidcDiscovery;
                 }
             }
 
-            return jsonWebKeySet;
+            var request = new HttpRequestMessage(HttpMethod.Get, oidcDiscoveryUri);
+            var client = httpClientFactory.CreateClient();
+            using (var response = await client.SendAsync(request))
+            {
+                // Handle the response
+                switch (response.StatusCode)
+                {
+                    case HttpStatusCode.OK:
+                        var result = await response.Content.ReadAsStringAsync();
+                        var oidcDiscovery = result.ToObject<OidcDiscovery>();
+                        var oidcDiscoveryValidUntil = DateTimeOffset.UtcNow.AddSeconds(expiresIn.Value);
+                        oidcDiscoveryCache.Add(oidcDiscoveryUri, (oidcDiscovery, oidcDiscoveryValidUntil));
+                        return oidcDiscovery;
+
+                    default:
+                        throw new Exception($"Error, Status Code OK expected. StatusCode={response.StatusCode}. OidcDiscoveryUri='{oidcDiscoveryUri}'.");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Call OIDC Discovery Keys endpoint or read the OIDC Discovery Keys from the cache.
+        /// </summary>
+        /// <param name="oidcDiscoveryUri">The OIDC Discovery uri, used to resolve the Keys endpoint. If not specified the default OIDC Discovery uri is used.</param>
+        /// <param name="expiresIn">The expires in seconds. If not specified the default expires in is used.</param>
+        /// <returns>Return OIDC Discovery Keys result.</returns>
+        public async Task<JsonWebKeySet> GetOidcDiscoveryKeysAsync(string oidcDiscoveryUri = null, int? expiresIn = null)
+        {
+            oidcDiscoveryUri = oidcDiscoveryUri ?? defaultOidcDiscoveryUri;
+            expiresIn = expiresIn ?? defaultExpiresIn;
+
+            if (jsonWebKeySetCache.ContainsKey(oidcDiscoveryUri))
+            {
+                (var jsonWebKeySet, var jsonWebKeySetValidUntil) = jsonWebKeySetCache[oidcDiscoveryUri];
+                if (jsonWebKeySetValidUntil >= DateTimeOffset.UtcNow)
+                {
+                    return jsonWebKeySet;
+                }
+            }
+
+            var oidcDiscovery = await GetOidcDiscoveryAsync(oidcDiscoveryUri, expiresIn);
+            var request = new HttpRequestMessage(HttpMethod.Get, oidcDiscovery.JwksUri);
+            var client = httpClientFactory.CreateClient();
+            using (var response = await client.SendAsync(request))
+            {
+                // Handle the response
+                switch (response.StatusCode)
+                {
+                    case HttpStatusCode.OK:
+                        var result = await response.Content.ReadAsStringAsync();
+                        var jsonWebKeySet = result.ToObject<JsonWebKeySet>();
+                        var jsonWebKeySetValidUntil = DateTimeOffset.UtcNow.AddSeconds(expiresIn.Value);
+                        jsonWebKeySetCache.Add(oidcDiscoveryUri, (jsonWebKeySet, jsonWebKeySetValidUntil));
+                        return jsonWebKeySet;
+
+                    default:
+                        throw new Exception($"Error, Status Code OK expected. StatusCode={response.StatusCode}. OidcDiscoveryJwksUri='{oidcDiscovery.JwksUri}'.");
+                }
+            }
+        }
+
+        bool isDisposed = false;
+        public void Dispose()
+        {
+            if (!isDisposed)
+            {
+                isDisposed = true;
+                cleanUpCancellationTokenSource.Cancel();
+            }
         }
     }
 }
