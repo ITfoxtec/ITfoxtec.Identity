4f27400d4c06b2d5102c16bb8b398df0d52ae985
Anders Revsgaard <anders@itfoxtec.com>
2019-07-25 15:16:40 +0200

    ITfoxtec.Identity to src

---
+++ test/ITfoxtec.Identity.UnitTest/JwtHandlerTests.cs
@@ -0,0 +1,20 @@
+using System.Security.Claims;
+using System.Threading.Tasks;
+using ITfoxtec.Identity.Tokens;
+using Xunit;
+
+namespace ITfoxtec.Identity.UnitTest
+{
+    public class JwtHandlerTests
+    {
+        [Fact]
+        public async Task Test1()
+        {
+            var testCertificate = await "test1".CreateSelfSignedCertificateAsync();
+            var testKey = await testCertificate.ToJsonWebKeyAsync(true);
+
+            var token = JwtHandler.CreateToken(testKey, "test-issuer", new[] { "test-aud" }, new[] { new Claim("sub", "test-user") });
+            var tokenString = token.ToJwtString();
+        }
+    }
+}
