d935597bbf3a2854a719ffa391584ff74a5996ec
Anders Revsgaard <anders@itfoxtec.com>
2019-08-20 11:46:01 +0200

    Add Authorization policy builder extensions

---
+++ src/Extensions/AuthorizationPolicyBuilderExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using ITfoxtec.Identity.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ITfoxtec.Identity
+{
+    /// <summary>
+    /// Authorization policy builder extensions.
+    /// </summary>
+    public static class AuthorizationPolicyBuilderExtensions
+    {
+        /// <summary>
+        /// Adds a <see cref="ScopeAuthorizationRequirement"/> to the current instance.
+        /// </summary>
+        /// <param name="policy">Extending <see cref="AuthorizationPolicyBuilder"/> policy.</param>
+        /// <param name="allowedValues">Values the scope must process one or more of for evaluation to succeed.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static AuthorizationPolicyBuilder RequireScope(this AuthorizationPolicyBuilder policy, params string[] allowedValues)
+        {
+            return RequireScope(policy, (IEnumerable<string>)allowedValues);
+
+        }
+
+        /// <summary>
+        /// Adds a <see cref="ScopeAuthorizationRequirement"/> to the current instance.
+        /// </summary>
+        /// <param name="policy">Extending <see cref="AuthorizationPolicyBuilder"/> policy.</param>
+        /// <param name="allowedValues">Values the scope must process one or more of for evaluation to succeed.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static AuthorizationPolicyBuilder RequireScope(this AuthorizationPolicyBuilder policy, IEnumerable<string> allowedValues)
+        {
+            if (allowedValues == null || !allowedValues.Any())
+            {
+                throw new ArgumentNullException(nameof(allowedValues), "The list of scope values is null or empty.");
+            }
+
+            policy.Requirements.Add(new ScopeAuthorizationRequirement(allowedValues));
+            return policy;
+        }
+    }
+}
