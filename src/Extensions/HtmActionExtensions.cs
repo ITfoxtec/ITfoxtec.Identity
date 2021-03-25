d21d57e8e56622130ed0c18f93fabe04ad11162a
Anders Revsgaard <anders@itfoxtec.com>
2021-03-25 12:00:47 +0100

    Expose HtmFormActionPageList and HtmRedirectActionPage methods. Support setting a title on HTML actions.

--- src/Extensions/HtmActionExtensions.cs
+++ src/Extensions/HtmActionExtensions.cs
@@ -6,60 +6,60 @@ using System.Threading.Tasks;
 namespace ITfoxtec.Identity
 {
     /// <summary>
-    /// Extension methods for HTML form actions.
+    /// Extension methods for HTML form and redirect actions.
     /// </summary>
-    public static class HtmFormActionExtensions
+    public static class HtmActionExtensions
     {
         /// <summary>
         /// Converts a Dictionary&lt;string, string&gt; to a HTML form action post page.
         /// </summary>
-        public static string ToHtmlPostPage(this Dictionary<string, string> items, string url)
+        public static string ToHtmlPostPage(this Dictionary<string, string> items, string url, string title = "OAuth 2.0")
         {
-            return string.Concat(HtmFormActionPageList(items, url, "post"));
+            return string.Concat(items.HtmFormActionPageList(url, "post", title: title));
         }
 
         /// <summary>
         /// Converts a Dictionary&lt;string, string&gt; to a HTML form action post page.
         /// </summary>
-        public static Task<string> ToHtmlPostPageAsync(this Dictionary<string, string> items, string url)
+        public static Task<string> ToHtmlPostPageAsync(this Dictionary<string, string> items, string url, string title = "OAuth 2.0")
         {
-            return Task.FromResult(ToHtmlPostPage(items, url));
+            return Task.FromResult(ToHtmlPostPage(items, url, title: title));
         }
 
         /// <summary>
         /// Converts a Dictionary&lt;string, string&gt; to a HTML form action get page.
         /// </summary>
-        public static string ToHtmlGetPage(this Dictionary<string, string> items, string url)
+        public static string ToHtmlGetPage(this Dictionary<string, string> items, string url, string title = "OAuth 2.0")
         {
-            return string.Concat(HtmFormActionPageList(items, url, "get"));
+            return string.Concat(items.HtmFormActionPageList(url, "get", title: title));
         }
 
         /// <summary>
         /// Converts a Dictionary&lt;string, string&gt; to a HTML form action get page.
         /// </summary>
-        public static Task<string> ToHtmlGetPageAsync(this Dictionary<string, string> items, string url)
+        public static Task<string> ToHtmlGetPageAsync(this Dictionary<string, string> items, string url, string title = "OAuth 2.0")
         {
-            return Task.FromResult(ToHtmlGetPage(items, url));
+            return Task.FromResult(ToHtmlGetPage(items, url, title: title));
         }
 
         /// <summary>
         /// Converts a Dictionary&lt;string, string&gt; to a HTML form action fragment page.
         /// </summary>
-        public static string ToHtmlFragmentPage(this Dictionary<string, string> items, string url)
+        public static string ToHtmlFragmentPage(this Dictionary<string, string> items, string url, string title = "OAuth 2.0")
         {
             var redirectUrl = QueryHelpers.AddQueryString(url, items).Replace('?', '#');
-            return HtmRedirectActionPageList(redirectUrl);
+            return redirectUrl.HtmRedirectActionPage(title: title);
         }
 
         /// <summary>
         /// Converts a Dictionary&lt;string, string&gt; to a HTML form action fragment page.
         /// </summary>
-        public static Task<string> ToHtmlFragmentPageAsync(this Dictionary<string, string> items, string url)
+        public static Task<string> ToHtmlFragmentPageAsync(this Dictionary<string, string> items, string url, string title = "OAuth 2.0")
         {
-            return Task.FromResult(ToHtmlFragmentPage(items, url));
+            return Task.FromResult(ToHtmlFragmentPage(items, url, title: title));
         }
 
-        private static IEnumerable<string> HtmFormActionPageList(Dictionary<string, string> items, string url, string method)
+        public static IEnumerable<string> HtmFormActionPageList(this Dictionary<string, string> items, string url, string method, string title = "OAuth 2.0")
         {
             yield return
 $@"<!DOCTYPE html>
@@ -67,7 +67,7 @@ $@"<!DOCTYPE html>
     <head>
         <meta charset=""utf-8"" />
         <meta http-equiv=""X-UA-Compatible"" content=""IE=edge"" />
-        <title>OAuth 2.0</title>
+        <title>{title}</title>
     </head>
     <body onload=""document.forms[0].submit()"">
         <noscript>
@@ -102,7 +102,7 @@ $@"            </div>
 </html>";
         }
 
-        private static string HtmRedirectActionPageList(string url)
+        public static string HtmRedirectActionPage(this string url, string title = "OAuth 2.0")
         {
             return
 $@"<!DOCTYPE html>
@@ -111,12 +111,9 @@ $@"<!DOCTYPE html>
         <meta charset=""utf-8"" />
         <meta http-equiv=""X-UA-Compatible"" content=""IE=edge"" />
         <meta http-equiv=""refresh"" content=""0;URL='{url}'"" />
-        <title>OAuth 2.0</title>
+        <title>{title}</title>
     </head>
     <body>
-        <p>
-            Please press <a href=""{url}"">here</a> to continue the proceed.
-        </p>
     </body>
 </html>";
         }
