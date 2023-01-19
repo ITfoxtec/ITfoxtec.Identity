207db36e26cc6ca3fa135fc08b0498b5dc238545
Anders Revsgaard <anders@itfoxtec.com>
2023-01-19 13:30:39 +0100

    Add GenerateCode, GenerateNumberCode and GenerateSimplePassword. And list Shuffle.

---
+++ test/ITfoxtec.Identity.UnitTest/RandomGeneratorTest.cs
@@ -0,0 +1,41 @@
+﻿using ITfoxtec.Identity.Util;
+using System.Linq;
+using Xunit;
+
+namespace ITfoxtec.Identity.UnitTest
+{
+    public class RandomGeneratorTest
+    {           
+        [Fact]
+        public void Generate6Test()
+        {
+            var result = RandomGenerator.Generate(6);
+
+            Assert.True(result.Count() >= 6);
+        }
+
+        [Fact]
+        public void GenerateCodeTest()
+        {
+            var result = RandomGenerator.GenerateCode(6);
+
+            Assert.True(result.Count() == 6);
+        }
+
+        [Fact]
+        public void GenerateNumberCodeTest()
+        {
+            var result = RandomGenerator.GenerateNumberCode(6);
+
+            Assert.True(result.Count() == 6);
+        }
+
+        [Fact]
+        public void GenerateSimplePasswordTest()
+        {
+            var result = RandomGenerator.GenerateSimplePassword(10);
+
+            Assert.True(result.Count() == 10);
+        }
+    }
+}
