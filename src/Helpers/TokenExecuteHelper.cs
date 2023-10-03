10420febc448ebdf7a910d86ab6b4796eccc3ca3
Anders Revsgaard <anders@itfoxtec.com>
2023-10-03 11:41:02 +0200

    Setting GrantType incorrectly bug resolved

---
+++ src/Helpers/TokenExecuteHelper.cs
@@ -0,0 +1,208 @@
+﻿using MSTokens = Microsoft.IdentityModel.Tokens;
+using ITfoxtec.Identity.Messages;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ITfoxtec.Identity.Models;
+using System.Security.Claims;
+using ITfoxtec.Identity.Tokens;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ITfoxtec.Identity.Helpers
+{
+    /// <summary>
+    /// Token helpers base.
+    /// </summary>
+    public class TokenExecuteHelper
+    {
+#if NET || NETCORE
+        private readonly IHttpClientFactory httpClientFactory;
+#else
+        private readonly HttpClient httpClient;
+#endif
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public TokenExecuteHelper(
+#if NET || NETCORE
+            IHttpClientFactory httpClientFactory)
+#else
+            HttpClient httpClient)
+#endif
+        {
+#if NET || NETCORE
+            this.httpClientFactory = httpClientFactory;
+#else
+            this.httpClient = httpClient;
+#endif
+        }
+
+        /// <summary>
+        /// Get access token with client credential grant.
+        /// </summary>
+        /// <typeparam name="Treq">Of the type TokenBaseRequest.</typeparam>
+        /// <typeparam name="Tres">Of the type TokenBaseResponse.</typeparam>
+        /// <param name="clientId">The client id.</param>
+        /// <param name="clientSecret">The client secret.</param>
+        /// <param name="tokenEndpoint">The token endpoint.</param>
+        /// <param name="tokenRequest">The token request.</param>
+        /// <returns>The <typeparam>Tres</typeparam> token response.</returns>
+        public async Task<Tres> ExecuteTokenRequestWithClientCredentialGrantAsync<Treq, Tres>(string clientId, string clientSecret, string tokenEndpoint, Treq tokenRequest = null) where Treq : TokenBaseRequest, new() where Tres : TokenBaseResponse
+        {
+            if (clientId.IsNullOrEmpty()) throw new ArgumentNullException(nameof(clientId));
+            if (clientSecret.IsNullOrEmpty()) throw new ArgumentNullException(nameof(clientSecret));
+            if (tokenEndpoint.IsNullOrEmpty()) throw new ArgumentNullException(nameof(tokenEndpoint));
+
+            if (tokenRequest == null)
+            {
+                tokenRequest = new Treq();
+            }
+
+            tokenRequest.GrantType = IdentityConstants.GrantTypes.ClientCredentials;
+            if (tokenRequest is TokenRequest tr)
+            {
+                tr.ClientId = clientId;
+            }
+
+            var request = new HttpRequestMessage(HttpMethod.Post, tokenEndpoint);
+            var nameValueCollection = tokenRequest.ToDictionary();
+            if (tokenRequest is TokenRequest)
+            {
+                nameValueCollection.AddToDictionary(new ClientCredentials
+                {
+                    ClientSecret = clientSecret,
+                });
+            }
+            else
+            {
+                nameValueCollection.AddToDictionary(new ClientIdAndCredentials
+                {
+                    ClientId = clientId,
+                    ClientSecret = clientSecret,
+                });
+            }
+            request.Content = new FormUrlEncodedContent(nameValueCollection);
+
+#if NET || NETCORE
+            var httpClient = httpClientFactory.CreateClient();
+#endif
+
+            using (var response = await httpClient.SendAsync(request))
+            {
+                try
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+
+                    var tokenResponse = result.ToObject<Tres>();
+                    tokenResponse.Validate();
+                    return tokenResponse;
+                }
+                catch (Exception ex)
+                {
+                    throw new ResponseException($"Error getting access token with client credentials. StatusCode={response.StatusCode}", ex);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Execute token request with assertion in client credential grant.
+        /// </summary>
+        /// <typeparam name="Treq">Of the type TokenBaseRequest.</typeparam>
+        /// <typeparam name="Tres">Of the type TokenBaseResponse.</typeparam>
+        /// <param name="jsonWebClientKey">The client key (certificate).</param>
+        /// <param name="clientId">The client id.</param>
+        /// <param name="tokenEndpoint">The token endpoint.</param>
+        /// <param name="tokenRequest">The token request.</param>
+        /// <param name="clientAssertionExpiresIn">The client assertion expires in.</param>
+        /// <returns>The <typeparam>Tres</typeparam> token response.</returns>
+        public Task<Tres> ExecuteTokenRequestWithAssertionClientCredentialGrantAsync<Treq, Tres>(JsonWebKey jsonWebClientKey, string clientId, string tokenEndpoint, Treq tokenRequest = null, int clientAssertionExpiresIn = 30) where Treq : TokenBaseRequest, new() where Tres : TokenBaseResponse
+        {
+            return ExecuteTokenRequestWithAssertionClientCredentialGrantAsync<Treq, Tres>(jsonWebClientKey.ToSecurityKey(), clientId, tokenEndpoint, tokenRequest: tokenRequest, clientAssertionExpiresIn: clientAssertionExpiresIn);
+        }
+
+        /// <summary>
+        /// Execute token request with assertion in client credential grant.
+        /// </summary>
+        /// <typeparam name="Treq">Of the type TokenBaseRequest.</typeparam>
+        /// <typeparam name="Tres">Of the type TokenBaseResponse.</typeparam>
+        /// <param name="certificateClientKey">The client key (certificate).</param>
+        /// <param name="clientId">The client id.</param>
+        /// <param name="tokenEndpoint">The token endpoint.</param>
+        /// <param name="tokenRequest">The token request.</param>
+        /// <param name="clientAssertionExpiresIn">The client assertion expires in.</param>
+        /// <returns>The <typeparam>Tres</typeparam> token response.</returns>
+        public Task<Tres> ExecuteTokenRequestWithAssertionClientCredentialGrantAsync<Treq, Tres>(X509Certificate2 certificateClientKey, string clientId, string tokenEndpoint, Treq tokenRequest = null, int clientAssertionExpiresIn = 30) where Treq : TokenBaseRequest, new() where Tres : TokenBaseResponse
+        {
+            return ExecuteTokenRequestWithAssertionClientCredentialGrantAsync<Treq, Tres>(new MSTokens.X509SecurityKey(certificateClientKey), clientId, tokenEndpoint, tokenRequest: tokenRequest, clientAssertionExpiresIn: clientAssertionExpiresIn);
+        }
+
+        /// <summary>
+        /// Execute token request with assertion in client credential grant.
+        /// </summary>
+        /// <typeparam name="Treq">Of the type TokenBaseRequest.</typeparam>
+        /// <typeparam name="Tres">Of the type TokenBaseResponse.</typeparam>
+        /// <param name="securityClientKey">The client key (certificate).</param>
+        /// <param name="clientId">The client id.</param>
+        /// <param name="tokenEndpoint">The token endpoint.</param>
+        /// <param name="tokenRequest">The token request.</param>
+        /// <param name="clientAssertionExpiresIn">The client assertion expires in.</param>
+        /// <returns>The <typeparam>Tres</typeparam> token response.</returns>
+        public async Task<Tres> ExecuteTokenRequestWithAssertionClientCredentialGrantAsync<Treq, Tres>(MSTokens.SecurityKey securityClientKey, string clientId, string tokenEndpoint, Treq tokenRequest = null, int clientAssertionExpiresIn = 30) where Treq : TokenBaseRequest, new() where Tres : TokenBaseResponse
+        {
+            if (securityClientKey == null) throw new ArgumentNullException(nameof(securityClientKey));
+            if (clientId.IsNullOrEmpty()) throw new ArgumentNullException(nameof(clientId));
+            if (tokenEndpoint.IsNullOrEmpty()) throw new ArgumentNullException(nameof(tokenEndpoint));
+            
+            if (tokenRequest == null)
+            {
+                tokenRequest = new Treq();
+            }
+
+            if (tokenRequest is TokenRequest tr)
+            {
+                tr.ClientId = clientId;
+            }
+
+            var clientAssertionClaims = new List<Claim>
+            {
+                new Claim(JwtClaimTypes.Subject, clientId),
+                new Claim(JwtClaimTypes.JwtId, Guid.NewGuid().ToString())
+            };
+
+            string algorithm = IdentityConstants.Algorithms.Asymmetric.RS256;
+            var key = securityClientKey is MSTokens.JsonWebKey jsonWebKey ? jsonWebKey.ToSecurityKey() : securityClientKey;
+            var token = JwtHandler.CreateToken(key, clientId, tokenEndpoint, clientAssertionClaims, expiresIn: clientAssertionExpiresIn, algorithm: algorithm);
+            var clientAssertionCredentials = new ClientAssertionCredentials
+            {
+                ClientAssertionType = IdentityConstants.ClientAssertionTypes.JwtBearer,
+                ClientAssertion = await token.ToJwtString()
+            };
+
+            var request = new HttpRequestMessage(HttpMethod.Post, tokenEndpoint);
+            var nameValueCollection = tokenRequest.ToDictionary().AddToDictionary(clientAssertionCredentials);
+            request.Content = new FormUrlEncodedContent(nameValueCollection);
+
+#if NET || NETCORE
+            var httpClient = httpClientFactory.CreateClient();
+#endif
+
+            using (var response = await httpClient.SendAsync(request))
+            {
+                try
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+
+                    var tokenResponse = result.ToObject<Tres>();
+                    tokenResponse.Validate();
+                    return tokenResponse;
+                }
+                catch (Exception ex)
+                {
+                    throw new ResponseException($"Error execute token request with client credentials. StatusCode={response.StatusCode}", ex);
+                }
+            }
+        }
+    }
+}
--- src/Helpers/TokenHelper.cs
+++ src/Helpers/TokenHelper.cs
@@ -5,9 +5,6 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using ITfoxtec.Identity.Discovery;
 using ITfoxtec.Identity.Models;
-using System.Security.Claims;
-using ITfoxtec.Identity.Tokens;
-using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 
 namespace ITfoxtec.Identity.Helpers
@@ -15,7 +12,7 @@ namespace ITfoxtec.Identity.Helpers
     /// <summary>
     /// Token helpers.
     /// </summary>
-    public class TokenHelper
+    public class TokenHelper : TokenExecuteHelper
     {
 #if NET || NETCORE
         private readonly IHttpClientFactory httpClientFactory;
@@ -33,7 +30,12 @@ namespace ITfoxtec.Identity.Helpers
 #else
             HttpClient httpClient,
 #endif
-            OidcDiscoveryHandler oidcDiscoveryHandler)
+            OidcDiscoveryHandler oidcDiscoveryHandler) : base (
+#if NET || NETCORE
+            httpClientFactory)
+#else
+            httpClient)
+#endif
         {
 #if NET || NETCORE
             this.httpClientFactory = httpClientFactory;
@@ -42,6 +44,7 @@ namespace ITfoxtec.Identity.Helpers
 #endif
             this.oidcDiscoveryHandler = oidcDiscoveryHandler;
         }
+
 
         /// <summary>
         /// Get access token with client credential grant.
@@ -56,158 +59,57 @@ namespace ITfoxtec.Identity.Helpers
             if (clientSecret.IsNullOrEmpty()) throw new ArgumentNullException(nameof(clientSecret));
 
             var tokenEndpoint = (await oidcDiscoveryHandler.GetOidcDiscoveryAsync()).TokenEndpoint;
-            return await GetAccessTokenWithClientCredentialGrantAsync(clientId, clientSecret, tokenEndpoint, scope.IsNullOrEmpty() ? null : new TokenRequest { Scope = scope });
+            var tokenResponse = await ExecuteTokenRequestWithClientCredentialGrantAsync<TokenRequest, TokenResponse>(clientId, clientSecret, tokenEndpoint, tokenRequest: scope.IsNullOrEmpty() ? null : new TokenRequest { GrantType = IdentityConstants.GrantTypes.ClientCredentials, Scope = scope });
+            return (tokenResponse.AccessToken, tokenResponse.ExpiresIn);
         }
 
         /// <summary>
-        /// Get access token with client credential grant.
-        /// </summary>
-        /// <typeparam name="Treq">Of the type TokenRequest.</typeparam>
-        /// <param name="clientId">The client id.</param>
-        /// <param name="clientSecret">The client secret.</param>
-        /// <param name="tokenEndpoint">The token endpoint.</param>
-        /// <param name="tokenRequest">The token request.</param>
-        /// <returns>The access token and expires in.</returns>
-        public async Task<(string, int?)> GetAccessTokenWithClientCredentialGrantAsync<Treq>(string clientId, string clientSecret, string tokenEndpoint, Treq tokenRequest = null) where Treq : TokenRequest
-        {
-            if (clientId.IsNullOrEmpty()) throw new ArgumentNullException(nameof(clientId));
-            if (clientSecret.IsNullOrEmpty()) throw new ArgumentNullException(nameof(clientSecret));
-            if (tokenEndpoint.IsNullOrEmpty()) throw new ArgumentNullException(nameof(tokenEndpoint));
-
-            var accessTokenRequest = tokenRequest ?? new TokenRequest();
-            accessTokenRequest.GrantType = IdentityConstants.GrantTypes.ClientCredentials;
-            accessTokenRequest.ClientId = clientId;
-
-            var clientCredentials = new ClientCredentials
-            {
-                ClientSecret = clientSecret,
-            };
-
-            var request = new HttpRequestMessage(HttpMethod.Post, tokenEndpoint);
-            var nameValueCollection = accessTokenRequest.ToDictionary().AddToDictionary(clientCredentials);
-            request.Content = new FormUrlEncodedContent(nameValueCollection);
-
-#if NET || NETCORE
-            var httpClient = httpClientFactory.CreateClient();
-#endif
-
-            using (var response = await httpClient.SendAsync(request))
-            {
-                try
-                {
-                    var result = await response.Content.ReadAsStringAsync();
-
-                    var tokenResponse = result.ToObject<TokenResponse>();
-                    tokenResponse.Validate();
-                    return (tokenResponse.AccessToken, tokenResponse.ExpiresIn);
-                }
-                catch (Exception ex)
-                {
-                    throw new ResponseException($"Error getting access token with client credentials. StatusCode={response.StatusCode}", ex);
-                }
-            }
-        }
-
-        /// <summary>
-        /// Get access token with client assertion grant.
-        /// </summary>
-        /// <param name="securityClientKey">The client key (certificate).</param>
-        /// <param name="clientId">The client id.</param>
-        /// <param name="scope">The scope.</param>
-        /// <returns>The access token and expires in.</returns>
-        public async Task<(string, int?)> GetAccessTokenWithClientAssertionGrantAsync(MSTokens.SecurityKey securityClientKey, string clientId, string scope = null, int clientAssertionExpiresIn = 30)
-        {
-            var tokenEndpoint = (await oidcDiscoveryHandler.GetOidcDiscoveryAsync()).TokenEndpoint;
-            return await GetAccessTokenWithClientAssertionGrantAsync(securityClientKey, clientId, tokenEndpoint, scope.IsNullOrEmpty() ? null : new TokenRequest { Scope = scope }, clientAssertionExpiresIn);
-        }
-
-        /// <summary>
-        /// Get access token with client assertion grant.
+        /// Get access token with assertion in client credential grant.
         /// </summary>
         /// <param name="jsonWebClientKey">The client key (certificate).</param>
         /// <param name="clientId">The client id.</param>
         /// <param name="scope">The scope.</param>
+        /// <param name="clientAssertionExpiresIn">The client assertion expires in.</param>
         /// <returns>The access token and expires in.</returns>
-        public async Task<(string, int?)> GetAccessTokenWithClientAssertionGrantAsync(JsonWebKey jsonWebClientKey, string clientId, string scope = null, int clientAssertionExpiresIn = 30)
+        public async Task<(string, int?)> GetAccessTokenWithAssertionClientCredentialGrantAsync(JsonWebKey jsonWebClientKey, string clientId, string scope = null, int clientAssertionExpiresIn = 30)
         {
             if (jsonWebClientKey == null) throw new ArgumentNullException(nameof(jsonWebClientKey));
 
             var tokenEndpoint = (await oidcDiscoveryHandler.GetOidcDiscoveryAsync()).TokenEndpoint;
-            return await GetAccessTokenWithClientAssertionGrantAsync(jsonWebClientKey.ToSecurityKey(), clientId, tokenEndpoint, scope.IsNullOrEmpty() ? null : new TokenRequest { Scope = scope }, clientAssertionExpiresIn);
+            var tokenResponse = await ExecuteTokenRequestWithAssertionClientCredentialGrantAsync<TokenRequest, TokenResponse>(jsonWebClientKey.ToSecurityKey(), clientId, tokenEndpoint, tokenRequest: scope.IsNullOrEmpty() ? null : new TokenRequest { GrantType = IdentityConstants.GrantTypes.ClientCredentials, Scope = scope }, clientAssertionExpiresIn);
+            return (tokenResponse.AccessToken, tokenResponse.ExpiresIn);
         }
 
         /// <summary>
-        /// Get access token with client assertion grant.
+        /// Get access token with assertion in client credential grant.
         /// </summary>
         /// <param name="certificateClientKey">The client key (certificate).</param>
         /// <param name="clientId">The client id.</param>
         /// <param name="scope">The scope.</param>
+        /// <param name="clientAssertionExpiresIn">The client assertion expires in.</param>
         /// <returns>The access token and expires in.</returns>
-        public async Task<(string, int?)> GetAccessTokenWithClientAssertionGrantAsync(X509Certificate2 certificateClientKey, string clientId, string scope = null, int clientAssertionExpiresIn = 30)
+        public async Task<(string, int?)> GetAccessTokenWithAssertionClientCredentialGrantAsync(X509Certificate2 certificateClientKey, string clientId, string scope = null, int clientAssertionExpiresIn = 30)
         {
             if (certificateClientKey == null) throw new ArgumentNullException(nameof(certificateClientKey));
 
             var tokenEndpoint = (await oidcDiscoveryHandler.GetOidcDiscoveryAsync()).TokenEndpoint;
-            return await GetAccessTokenWithClientAssertionGrantAsync(new MSTokens.X509SecurityKey(certificateClientKey), clientId, tokenEndpoint, scope.IsNullOrEmpty() ? null : new TokenRequest { Scope = scope }, clientAssertionExpiresIn);
+            var tokenResponse = await ExecuteTokenRequestWithAssertionClientCredentialGrantAsync<TokenRequest, TokenResponse>(certificateClientKey, clientId, tokenEndpoint, tokenRequest: scope.IsNullOrEmpty() ? null : new TokenRequest { GrantType = IdentityConstants.GrantTypes.ClientCredentials, Scope = scope }, clientAssertionExpiresIn);
+            return (tokenResponse.AccessToken, tokenResponse.ExpiresIn);
         }
 
         /// <summary>
-        /// Get access token with client assertion grant.
+        /// Get access token with assertion in client credential grant.
         /// </summary>
-        /// <typeparam name="Treq">Of the type TokenRequest.</typeparam>
         /// <param name="securityClientKey">The client key (certificate).</param>
         /// <param name="clientId">The client id.</param>
-        /// <param name="tokenEndpoint">The token endpoint.</param>
-        /// <param name="tokenRequest">The token request.</param>
+        /// <param name="scope">The scope.</param>
+        /// <param name="clientAssertionExpiresIn">The client assertion expires in.</param>
         /// <returns>The access token and expires in.</returns>
-        public async Task<(string, int?)> GetAccessTokenWithClientAssertionGrantAsync<Treq>(MSTokens.SecurityKey securityClientKey, string clientId, string tokenEndpoint, Treq tokenRequest = null, int clientAssertionExpiresIn = 30) where Treq : TokenRequest
+        public async Task<(string, int?)> GetAccessTokenWithAssertionClientCredentialGrantAsync(MSTokens.SecurityKey securityClientKey, string clientId, string scope = null, int clientAssertionExpiresIn = 30)
         {
-            if (securityClientKey == null) throw new ArgumentNullException(nameof(securityClientKey));
-            if (clientId.IsNullOrEmpty()) throw new ArgumentNullException(nameof(clientId));
-            if (tokenEndpoint.IsNullOrEmpty()) throw new ArgumentNullException(nameof(tokenEndpoint));
-
-            var accessTokenRequest = tokenRequest ?? new TokenRequest();
-            accessTokenRequest.GrantType = IdentityConstants.GrantTypes.ClientCredentials;
-            accessTokenRequest.ClientId = clientId;
-
-            var clientAssertionClaims = new List<Claim>
-            {
-                new Claim(JwtClaimTypes.Subject, clientId),
-                new Claim(JwtClaimTypes.JwtId, Guid.NewGuid().ToString())
-            };
-
-            string algorithm = IdentityConstants.Algorithms.Asymmetric.RS256;
-            var key = securityClientKey is MSTokens.JsonWebKey jsonWebKey ? jsonWebKey.ToSecurityKey() : securityClientKey;
-            var token = JwtHandler.CreateToken(key, clientId, tokenEndpoint, clientAssertionClaims, expiresIn: clientAssertionExpiresIn, algorithm: algorithm);
-            var clientAssertionCredentials = new ClientAssertionCredentials
-            {
-                ClientAssertionType = IdentityConstants.ClientAssertionTypes.JwtBearer,
-                ClientAssertion = await token.ToJwtString()
-            };
-
-            var request = new HttpRequestMessage(HttpMethod.Post, tokenEndpoint);
-            var nameValueCollection = accessTokenRequest.ToDictionary().AddToDictionary(clientAssertionCredentials);
-            request.Content = new FormUrlEncodedContent(nameValueCollection);
-
-#if NET || NETCORE
-            var httpClient = httpClientFactory.CreateClient();
-#endif
-
-            using (var response = await httpClient.SendAsync(request))
-            {
-                try
-                {
-                    var result = await response.Content.ReadAsStringAsync();
-
-                    var tokenResponse = result.ToObject<TokenResponse>();
-                    tokenResponse.Validate();
-                    return (tokenResponse.AccessToken, tokenResponse.ExpiresIn);
-                }
-                catch (Exception ex)
-                {
-                    throw new ResponseException($"Error getting access token with client credentials. StatusCode={response.StatusCode}", ex);
-                }
-            }
+            var tokenEndpoint = (await oidcDiscoveryHandler.GetOidcDiscoveryAsync()).TokenEndpoint;
+            var tokenResponse = await ExecuteTokenRequestWithAssertionClientCredentialGrantAsync<TokenRequest, TokenResponse>(securityClientKey, clientId, tokenEndpoint, tokenRequest: scope.IsNullOrEmpty() ? null : new TokenRequest { GrantType = IdentityConstants.GrantTypes.ClientCredentials, Scope = scope }, clientAssertionExpiresIn);
+            return (tokenResponse.AccessToken, tokenResponse.ExpiresIn);
         }
     }
 }
