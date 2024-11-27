90d505e351659316dc15b5643bfa9fb71fff71d9
Anders Revsgaard <anders@itfoxtec.com>
2024-11-27 14:00:08 +0100

    Use X509CertificateLoader in .NET 9 instead of X509Certificate2.

--- src/Util/CertificateUtil.cs
+++ src/Util/CertificateUtil.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+#if NETSTANDARD || NET60 || NET70 || NET80
 using System.Security;
+#endif
 using System.Security.Cryptography.X509Certificates;
 
 namespace ITfoxtec.Identity.Util
@@ -10,39 +12,73 @@ namespace ITfoxtec.Identity.Util
         {
             if (path.IsNullOrWhiteSpace()) throw new ArgumentNullException(nameof(path));
 
+#if NETSTANDARD || NET60 || NET70 || NET80
             return new X509Certificate2(path);
+#else
+            return X509CertificateLoader.LoadCertificateFromFile(path);
+#endif
         }
 
-        public static X509Certificate2 Load(string path, string password, X509KeyStorageFlags keyStorageFlags = X509KeyStorageFlags.DefaultKeySet)
+        public static X509Certificate2 Load(string path, string password)
+        {
+            if (string.IsNullOrWhiteSpace(path)) throw new ArgumentNullException(nameof(path));
+            if (string.IsNullOrWhiteSpace(password)) throw new ArgumentNullException(nameof(password));
+
+#if NETSTANDARD || NET60 || NET70 || NET80
+            return new X509Certificate2(path, password);
+#else
+            return X509CertificateLoader.LoadPkcs12FromFile(path, password);
+#endif
+        }
+
+        public static X509Certificate2 Load(string path, string password, X509KeyStorageFlags keyStorageFlags)
         {
             if (path.IsNullOrWhiteSpace()) throw new ArgumentNullException(nameof(path));
             if (password.IsNullOrWhiteSpace()) throw new ArgumentNullException(nameof(password));
 
+#if NETSTANDARD || NET60 || NET70 || NET80
             return new X509Certificate2(path, password, keyStorageFlags);
-        }
-        public static X509Certificate2 Load(string path, SecureString password, X509KeyStorageFlags keyStorageFlags = X509KeyStorageFlags.DefaultKeySet)
-        {
-            if (path.IsNullOrWhiteSpace()) throw new ArgumentNullException(nameof(path));
-            if (password == null) throw new ArgumentNullException(nameof(password));
-
-            return new X509Certificate2(path, password, keyStorageFlags);
+#else
+            return X509CertificateLoader.LoadPkcs12FromFile(path, password, keyStorageFlags);
+#endif
         }
 
         public static X509Certificate2 LoadBytes(string certificate)
         {
-            if (certificate.IsNullOrWhiteSpace()) throw new ArgumentNullException(nameof(certificate));
+            if (string.IsNullOrWhiteSpace(certificate)) throw new ArgumentNullException(nameof(certificate));
 
             var encoding = new System.Text.UTF8Encoding();
+#if NETSTANDARD || NET60 || NET70 || NET80
             return new X509Certificate2(encoding.GetBytes(certificate));
+#else
+            return X509CertificateLoader.LoadCertificate(encoding.GetBytes(certificate));
+#endif
         }
 
-        public static X509Certificate2 LoadBytes(string certificate, string password, X509KeyStorageFlags keyStorageFlags = X509KeyStorageFlags.DefaultKeySet)
+        public static X509Certificate2 LoadBytes(string certificate, string password)
         {
-            if (certificate.IsNullOrWhiteSpace()) throw new ArgumentNullException(nameof(certificate));
+            if (string.IsNullOrWhiteSpace(certificate)) throw new ArgumentNullException(nameof(certificate));
             if (password == null) throw new ArgumentNullException(nameof(password));
 
             var encoding = new System.Text.UTF8Encoding();
+#if NETSTANDARD || NET60 || NET70 || NET80
+            return new X509Certificate2(encoding.GetBytes(certificate), password);
+#else
+            return X509CertificateLoader.LoadPkcs12(encoding.GetBytes(certificate), password);
+#endif
+        }
+
+        public static X509Certificate2 LoadBytes(string certificate, string password, X509KeyStorageFlags keyStorageFlags)
+        {
+            if (string.IsNullOrWhiteSpace(certificate)) throw new ArgumentNullException(nameof(certificate));
+            if (password == null) throw new ArgumentNullException(nameof(password));
+
+            var encoding = new System.Text.UTF8Encoding();
+#if NETSTANDARD || NET60 || NET70 || NET80
             return new X509Certificate2(encoding.GetBytes(certificate), password, keyStorageFlags);
+#else
+            return X509CertificateLoader.LoadPkcs12(encoding.GetBytes(certificate), password);
+#endif
         }
 
         public static X509Certificate2 Load(StoreName name, StoreLocation location, X509FindType type, string findValue)
